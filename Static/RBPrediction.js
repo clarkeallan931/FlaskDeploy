ca409a7f5d2a18ebd1643310b940778d9ed82f28
clarkeallan931 <85319839+clarkeallan931@users.noreply.github.com>
2024-04-25 10:38:32 -0500

    Add files via upload

---
+++ Static/RBPrediction.js
@@ -0,0 +1,147 @@
+let year = ['2019', '2020', '2021', '2022', '2023', '2024'];
+let url = '/RBYearlyPoints.json'; 
+
+function search() {
+    let searchInput = document.getElementById('searchInput');
+    let searchText = searchInput.value.toLowerCase();
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            
+            console.log(data);
+            searchAndPlot(data, searchText);
+        })
+        .catch(error => {
+            console.error('Error fetching or processing data:', error);
+            openPopup();
+        });
+}
+
+function searchAndPlot(data, searchText) {
+    let keyFound = Object.keys(data).find(key => key.toLowerCase().includes(searchText.toLowerCase()));
+
+    if (keyFound) {
+        let values = data[keyFound];
+
+        let trace1 = {
+            x: year,
+            y: values,
+            type: 'bar',
+            marker: {
+                color: ['blue', 'blue', 'blue', 'blue', 'blue', 'red'],
+                
+                line: {
+                        color: 'black', 
+                        width: 1.5 
+                    },
+                    
+                    text: values.map(String),
+                    textposition: 'auto'
+            }
+        };
+
+        let plotData = [trace1];
+
+        let layout = {
+            title: {
+                text: `${keyFound} Fantasy Points By Year`,
+                font: {
+                    size: 16
+                }
+            },
+            xaxis: {
+                title: 'Year'
+            },
+            yaxis: {
+                title: 'Fantasy Points'
+            }
+        };
+
+        Plotly.newPlot('plot3', plotData, layout);
+    } else {
+        console.log('No matching key found');
+        openPopup();
+    }
+}
+
+function searchSecondGraph() {
+    let searchInput = document.getElementById('searchInput2');
+    let searchText = searchInput.value.toLowerCase();
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            
+            console.log(data);
+            searchAndPlotSecondGraph(data, searchText);
+        })
+        .catch(error => {
+            console.error('Error fetching or processing data:', error);
+            openPopup();
+        });
+}
+
+function searchAndPlotSecondGraph(data, searchText) {
+    let keyFound = Object.keys(data).find(key => key.toLowerCase().includes(searchText.toLowerCase()));
+
+    if (keyFound) {
+        let values = data[keyFound];
+
+        let trace = {
+            x: year,
+            y: values,
+            type: 'bar',
+            marker: {
+                color: ['blue', 'blue', 'blue', 'blue', 'blue', 'red'],
+                line: {
+                    color: 'black',
+                    width: 1.5
+                },
+                text: values.map(String),
+                textposition: 'auto'
+            }
+        };
+
+        let plotData = [trace];
+
+        let layout = {
+            title: {
+                text: `${keyFound} Fantasy Points By Year`,
+                font: {
+                    size: 16
+                }
+            },
+            xaxis: {
+                title: 'Year'
+            },
+            yaxis: {
+                title: 'Fantasy Points'
+            }
+        };
+
+        Plotly.newPlot('plot4', plotData, layout); 
+    } else {
+        console.log('No matching key found for the second graph');
+        openPopup();
+    }
+}
+
+
+function openPopup() {
+    document.getElementById('popupContainer').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popupContainer').style.display = 'none';
+}
---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, send_from_directory, jsonify
+
+UPLOAD_FOLDER = 'mysite/static/uploads'
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('Home.html')
+
+
+
+@app.route('/QB')#button to QB page 
+def qb_index():
+    return render_template('QBPointsFlask.html')
+
+
+@app.route('/RB')#button to RB page 
+def rb_index():
+    return render_template('RBPointsFlask.html')
+
+@app.route('/WR')#button to WR page
+def wr_index():
+    return render_template('WRPointsFlask.html')
+
+@app.route('/predictQB')#button to WR page
+def predict():
+    return render_template('QBPredictionFlask.html')
+
+@app.route('/predictRB')#button to WR page
+def predictRB():
+    return render_template('RBPredictionFlask.html')
+
+@app.route('/predictWR')#button to WR page
+def predictWR():
+    return render_template('WRPredictionFlask.html')
+
+
+@app.route('/QB5.json')#import QB last 5 games data 
+def get_QB5_data():
+    return send_from_directory('static', 'QB5.json')
+
+@app.route('/QBPointData.json') #import QB weekly point data 
+def get_QBPoints():
+    return send_from_directory('static', 'QBPointData.json')
+
+@app.route('/QBYear.json') #import QB yearly data 
+def get_QBYear_data():
+    return send_from_directory('static', 'QBYear.json')
+
+@app.route('/RB5.json')#import RB last 5 games data 
+def get_RB5_data():
+    return send_from_directory('static', 'RB5.json')
+
+@app.route('/RBPointData.json')#import RB weekly point data 
+def get_RBPointData():
+    return send_from_directory('static', 'RBPointData.json')
+
+@app.route('/RBYear.json')#import RB yearly data 
+def get_RBYear_data():
+    return send_from_directory('static', 'RBYear.json')
+
+@app.route('/WR5.json')#import WR last 5 games data 
+def get_WR5_data():
+    return send_from_directory('static', 'WR5.json')
+
+@app.route('/WRPointData.json')#import RB weekly point data 
+def get_WRPointData():
+    return send_from_directory('static', 'WRPointData.json') 
+
+@app.route('/WRYear.json')#import WR yearly data
+def get_WRYear_data():
+    return send_from_directory('static', 'WRYear.json')
+
+
+@app.route('/QBYearlyPoints.json') #import QB weekly point data 
+def get_QBYearlyPoints():
+    return send_from_directory('static', 'QBYearlyPoints.json')
+
+@app.route('/RBYearlyPoints.json') #import QB weekly point data 
+def get_RBYearlyPoints():
+    return send_from_directory('static', 'RBYearlyPoints.json')
+
+@app.route('/WRYearlyPoints.json') #import QB weekly point data 
+def get_WRYearlyPoints():
+    return send_from_directory('static', 'WRYearlyPoints.json')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
